345b6bc623077c1c8fc52c48807b74e7f3a28239
NeyNasc <claudineydelima@gmail.com>
2016-12-11 16:43:57 -0200

    Criando testes de servidor.

--- src/main/java/br/com/alura/loja/Servidor.java
+++ src/main/java/br/com/alura/loja/Servidor.java
@@ -8,12 +8,18 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class Servidor {
-	public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = inicializaServidor();
+        System.out.println("Servidor rodando");
+        System.in.read();
+        server.stop();
+    }
+
+	public static HttpServer inicializaServidor() {
 		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
-		URI uri = URI.create("http://localhost:8080/");
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
-		System.out.println("Servidor Rodando");
-		System.in.read();
-		server.stop();
+        URI uri = URI.create("http://localhost:8080/");
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		return server;
 	}
 }
