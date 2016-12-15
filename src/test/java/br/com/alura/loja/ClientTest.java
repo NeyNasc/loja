6fe06097d0088dffaea491a594861f288f526361
NeyNasc <claudineydelima@gmail.com>
2016-12-14 22:29:00 -0200

    Alterando URI no metódo de teste.

--- src/test/java/br/com/alura/loja/ClientTest.java
+++ src/test/java/br/com/alura/loja/ClientTest.java
@@ -32,12 +32,12 @@ public class ClientTest {
     }
 	
     @Test
-    public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+    public void testaQueBuscarUmCarrinhoTrasUmCarrinho() {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080");
-        String conteudo = target.path("/carrinhos").request().get(String.class);
-        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
-        Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+        String conteudo = target.path("/carrinhos/1").request().get(String.class);
+        Carrinho fromXML = (Carrinho) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Rua Vergueiro 3185, 8 andar",fromXML.getRua());
     }
 
 }
